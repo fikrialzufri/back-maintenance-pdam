5f730b8a376426bb9b039562245c3b30279ac943
Fikri Zufri <fikri@student.spb.ac.id>
2022-04-23 16:38:47 +0800

    update

--- app/Http/Controllers/Api/PelaksanaanPekerjaanController.php
+++ app/Http/Controllers/Api/PelaksanaanPekerjaanController.php
@@ -313,8 +313,8 @@ class PelaksanaanPekerjaanController extends Controller
         }
 
         if (request()->user()->hasRole('staf-pengawas')) {
-            $rekanan = auth()->user()->karyawan_list_rekanan;
-            if (in_array($rekanan_id, $rekanan)) {
+            $listRekanan = auth()->user()->karyawan_list_rekanan;
+            if (in_array($listRekanan, $rekanan_id)) {
                 return 1;
                 $data->status = $status;
                 $data->save();
