f2db0b431e3417a58933f6608f13fcc9c1ed1f77
Fikri Zufri <fikri@student.spb.ac.id>
2022-04-24 01:04:54 +0800

    tagihan

--- app/Http/Controllers/Api/TagihanController.php
+++ app/Http/Controllers/Api/TagihanController.php
@@ -58,25 +58,24 @@ class TagihanController extends Controller
             $query->whereBetween('created_at', [$start, $end]);
         }
 
-        try {
-            $data = $query->orderBy('status', 'ASC')->orderBy('created_at')->get();
-            if (count($data) == 0) {
-                $message = 'Data List Tagihan Belum Ada';
-            }
-            foreach ($data as $key => $value) {
-                $result[$key] = [
-                    'id' =>  $value->id,
-                    'nomor_pekerjaan' =>  $value->nomor_pekerjaan,
-                    'slug' =>  $value->slug,
-                    'lokasi_aduan' =>  $value->lokasi,
-                    'lokasi_pekerjaan' =>  $value->lokasi_pekerjaan,
-                    'status' =>  $value->status,
-                    'created_at' =>  $value->created_at,
-                    'status_mobile' =>  $value->status_mobile,
-                ];
-            }
-            return $this->sendResponse($result, $message, 200);
-        } catch (\Throwable $th) {
+        $data = $query->orderBy('status', 'ASC')->orderBy('created_at')->get();
+        if (count($data) == 0) {
+            $message = 'Data List Tagihan Belum Ada';
+        }
+        foreach ($data as $key => $value) {
+            $result[$key] = [
+                'id' =>  $value->id,
+                'nomor_pekerjaan' =>  $value->nomor_pekerjaan,
+                'slug' =>  $value->slug,
+                'lokasi_aduan' =>  $value->lokasi,
+                'lokasi_pekerjaan' =>  $value->lokasi_pekerjaan,
+                'status' =>  $value->status,
+                'created_at' =>  $value->created_at,
+                'status_mobile' =>  $value->status_mobile,
+            ];
+        }
+        return $this->sendResponse($result, $message, 200);
+        try { } catch (\Throwable $th) {
             $response = [
                 'success' => false,
                 'message' => $message,
