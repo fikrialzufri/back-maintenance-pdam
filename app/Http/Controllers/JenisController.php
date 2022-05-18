76e2ff50b18bba728db0ce1766d5300298a9f65e
Fikri Zufri <fikri@student.spb.ac.id>
2022-05-18 17:10:19 +0800

    sort by di jenis dan kategori

--- app/Http/Controllers/JenisController.php
+++ app/Http/Controllers/JenisController.php
@@ -12,6 +12,7 @@ class JenisController extends Controller
     public function __construct()
     {
         $this->route = 'jenis';
+        $this->sort = 'nama';
         $this->middleware('permission:view-' . $this->route, ['only' => ['index', 'show']]);
         $this->middleware('permission:create-' . $this->route, ['only' => ['create', 'store']]);
         $this->middleware('permission:edit-' . $this->route, ['only' => ['edit', 'update']]);
