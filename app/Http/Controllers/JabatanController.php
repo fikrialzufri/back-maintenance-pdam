ffd3c1b54b552cba9222f920a7535eec3a33fc09
Fikri Zufri <fikrizufri@gmail.com>
2023-11-24 20:25:26 +0800

    :hammer: mntap

--- app/Http/Controllers/JabatanController.php
+++ app/Http/Controllers/JabatanController.php
@@ -14,6 +14,7 @@ class JabatanController extends Controller
     public function __construct()
     {
         $this->route = 'jabatan';
+        $this->sort = 'nama';
         $this->middleware('permission:view-' . $this->route, ['only' => ['index', 'show']]);
         $this->middleware('permission:create-' . $this->route, ['only' => ['create', 'store']]);
         $this->middleware('permission:edit-' . $this->route, ['only' => ['edit', 'update']]);
