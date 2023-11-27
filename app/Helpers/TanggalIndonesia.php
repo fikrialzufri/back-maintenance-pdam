f73319826fec3eb5ffa488812cccc005fa8bc4fc
Fikri Zufri <fikrizufri@gmail.com>
2023-11-27 20:15:05 +0800

    :hammer: bulan

--- app/Helpers/TanggalIndonesia.php
+++ app/Helpers/TanggalIndonesia.php
@@ -64,7 +64,7 @@ function tanggal_indonesia_terbilang($tgl, $tampil_hari = true, $koma = true)
     } else {
         $text .= " ";
     }
-    $text .= terbilang($tanggal) . " " . $bulan . " tahun " . $tahun;
+    $text .= terbilang($tanggal) . " bulan " . $bulan . " tahun " . $tahun;
     return $text;
 }
 function capital_tanggal_indonesia($tgl, $tampil_hari = true)
