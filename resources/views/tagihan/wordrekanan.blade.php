3ec76391d40229652ed8e7e1a68f890517b74e53
Fikri Zufri <fikrizufri@gmail.com>
2023-11-30 21:32:35 +0800

    :hammer: mantap

--- resources/views/tagihan/wordrekanan.blade.php
+++ resources/views/tagihan/wordrekanan.blade.php
@@ -162,20 +162,20 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td
-                                            style="width: 58.48%;padding: 0cm 5.4pt;border-image: initial;vertical-align: top;">
+                                        <td style="width: 58.48%;padding: 0cm 1.4pt;vertical-align: top;">
                                             <p
                                                 style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
                                                 &nbsp;</p>
                                         </td>
-                                        <td style="width: 41.52%;padding: 0cm 5.4pt;vertical-align: top;">
+                                        <td style="width: 41.52%;padding: 0cm 1.4pt;vertical-align: top;">
+
                                             <p
                                                 style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
-                                                &nbsp;</p>
+                                                Direktur</p>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td style="width: 58.48%;padding: 0cm 5.4pt;vertical-align: top;">
+                                        <td style="width: 58.48%;padding: 0cm 1pt;vertical-align: top;">
                                             <p
                                                 style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
                                                 &nbsp;</p>
@@ -191,21 +191,6 @@
                                                     @endif
                                                 @endif
                                             </div>
-                                            <p
-                                                style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
-                                                &nbsp;</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="width: 58.48%;padding: 0cm 5.4pt;vertical-align: top;">
-                                            <p
-                                                style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
-                                                &nbsp;</p>
-                                        </td>
-                                        <td style="width: 41.52%;padding: 0cm 5.4pt;vertical-align: top;">
-                                            <p
-                                                style='margin-right:0cm;margin-left:0cm;font-size:16px;font-family:"Times New Roman",serif;margin:0cm;margin-top:0cm;margin-bottom:8.0pt;text-align:center;line-height:105%;'>
-                                                Direktur</p>
                                         </td>
 
                                     </tr>
